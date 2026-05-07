22a9ddf94f6486f1f81cb61241541cbed1163ad3
darja272005-glitch <darja272005@gmail.com>
2026-05-07 15:31:57 +0300

    Add files via upload

---
+++ Pokemo_Shop.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Pokemo_Shop {
+    private JLabel pokemon_shop;
+    private JTextField pokemonNameField;
+    private JComboBox comboBox1;
+    private JList list1;
+    private JButton scoreButton;
+    private JButton removeButton;
+    private JButton button1;
+    private JTextField clientNameField;
+    private JTextField HPTextField;
+}
